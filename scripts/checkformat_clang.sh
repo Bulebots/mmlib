fa01f2addc9fa3d8617ea20009c6c2b79d3335e7
Miguel Sánchez de León Peque <peque@neosit.es>
2019-04-08 21:56:06 +0200

    Check format in CI

---
+++ scripts/checkformat_clang.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+output=$(mktemp)
+errors=$(mktemp)
+find ./ -type f -regex ".*\.\(c\|h\)$" \
+	-not -path "./printf/*" | while read fname
+do
+	clang-format "$fname" > "$output"
+	git diff --color "$fname" "$output" >> "$errors"
+done
+rm -f "$output"
+if [ -s "$errors" ]
+then
+	echo -e "\033[0;31mFORMATTING ERRORS!\033[0m"
+	cat "$errors"
+	rm -f "$errors"
+	exit 1
+fi
+rm -f "$errors"
